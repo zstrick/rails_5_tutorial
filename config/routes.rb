eea9ca1f08751de0d0a1174f7cb187b093215bb0
Zack Strickland <zack.a.strickland@gmail.com>
2017-09-14 09:03:37 -0400

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,16 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
